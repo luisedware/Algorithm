8cf9683631372b44fad399a03d9e0840cf7947dd
sebastiankennedy <sebastiankennedy@foxmail.com>
2020-03-11 08:24:28 +0800

    SingleLinkedList

--- PHP/LinearList/LinkedList/SingleLinkedList.php
+++ PHP/LinearList/LinkedList/SingleLinkedList.php
@@ -2,23 +2,112 @@
 
 namespace LinearList\LinkedList;
 
+/**
+ * 单向链表
+ *
+ * Class SingleLinkedList
+ *
+ * @package LinearList\LinkedList
+ */
 class SingleLinkedList
 {
+    /**
+     * 单链表头结点
+     *
+     *
+     * @var SingleLinkedListNode|null
+     */
     public ?SingleLinkedListNode $head;
+    /**
+     * 单链表的长度
+     *
+     * @var int
+     */
     public int $length;
 
-    public function __construct($head = null)
+    /**
+     * 初始化单链表
+     *
+     * SingleLinkedList constructor.
+     *
+     * @param  SingleLinkedListNode|null  $head
+     */
+    public function __construct(?SingleLinkedListNode $head = null)
     {
-        if (null === $head) {
-            $this->head = new SingleLinkedListNode();
+        $this->head = $head;
+        $this->length = 0;
+    }
+
+    /**
+     * 入栈
+     *
+     * @param $data
+     *
+     * @return $this
+     */
+    public function push($data)
+    {
+        // 新增单链表节点
+        $new_node = new SingleLinkedListNode($data);
+
+        // 判断是否为空链表
+        if ($this->head === null) {
+            // 添加头结点
+            $this->head = $new_node;
         } else {
-            if (!$head instanceof SingleLinkedListNode) {
-                return false;
+            $current_node = $this->head;
+            for ($i = 0; $i < $this->length - 1; $i++) {
+                $current_node = $current_node->next;
             }
 
-            $this->head = $head;
+            // 设置单链表节点的数据域与节点域
+            $new_node->next = $current_node->next;
+            $current_node->next = $new_node;
         }
 
-        $this->length = 0;
+        // 单链表长度增一
+        $this->length++;
+
+        return $this;
+    }
+
+    /**
+     * 出栈
+     *
+     * @return $this
+     */
+    public function pop()
+    {
+        if (!$this->head) {
+            return $this;
+        }
+
+        if (!$this->head->next) {
+            $this->head = null;
+            $this->length--;
+
+            return $this;
+        }
+
+        $prev_node = $current_node = $this->head;
+        for ($i = 0; $i < $this->length - 1; $i++) {
+            $prev_node = $current_node;
+            $current_node = $current_node->next;
+        }
+
+        $prev_node->next = null;
+        $this->length--;
+
+        return $this;
+    }
+
+    /**
+     * 获取
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return $this->length;
     }
 }
--- PHP/LinearList/LinkedList/main.php
+++ PHP/LinearList/LinkedList/main.php
@@ -3,10 +3,24 @@
 require_once '../../vendor/autoload.php';
 
 use LinearList\LinkedList\SingleLinkedList;
-use LinearList\LinkedList\SingleLinkedListNode;
 
 $list = new SingleLinkedList();
+var_dump($list); // 空链表
+
+$list->push('Hello');
+var_dump($list); // 头结点
+
+$list->push('world');
+var_dump($list); // 第二个节点
+
+$list->push('sebastian')->push('kennedy');
 var_dump($list);
 
-$node = new SingleLinkedListNode('Hello');
-var_dump($node);
+$list->pop();
+var_dump($list);
+
+$list->pop();
+var_dump($list);
+
+$list->pop();
+var_dump($list);
