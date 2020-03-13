2e1fb1a7afd80fcad80ca4431c9a0bcd801ea647
sebastiankennedy <sebastiankennedy@foxmail.com>
2020-03-14 06:41:32 +0800

    Stack

--- PHP/LinearList/Stack/StackHandle.php
+++ PHP/LinearList/Stack/StackHandle.php
@@ -9,4 +9,13 @@ interface StackHandle
 
     // 出栈
     public function pop();
+
+    // 返回最后入栈的元素
+    public function peek();
+
+    // 返回栈中元素数量
+    public function count();
+
+    // 返回栈的大小
+    public function length();
 }
